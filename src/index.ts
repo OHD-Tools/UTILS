c87ed01fa96c274bc9fafa513e52d96d411fea93
Ethan Snow <bombitmanbomb@gmail.com>
2025-02-28 17:52:51 -0500

    fix exports

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
+export { default as Factions } from './definitions/Factions';
+export { default as Maps } from './definitions/Maps';
+
+export { GameINI } from './utils/GameINI';
+
+export type { MapQueryProps } from './utils/MapQuery';
 export { default as MapQuery } from './utils/MapQuery';
