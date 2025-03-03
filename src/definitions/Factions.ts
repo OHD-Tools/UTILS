7826d0779e074655ebfbfc4786cbb2e1e46b95ea
Ethan Snow <bombitmanbomb@gmail.com>
2025-03-03 16:16:36 -0500

    0.12.2

--- src/definitions/Factions.ts
+++ src/definitions/Factions.ts
@@ -3,20 +3,39 @@
  */
 enum Factions {
   /**Default Faction for the Map */
-  DEFAULT = 'default',
+  DEFAULT = 'DEFAULT',
+  //? Modern
   BP_HDFactionInfo_PMC = 'BP_HDFactionInfo_PMC',
   BP_HDFactionInfo_Insurgents = 'BP_HDFactionInfo_Insurgents',
-  BP_HDFactionInfo_Russians = 'BP_HDFactionInfo_Russians',
-  BP_HDFactionInfo_TG_BluFor = 'BP_HDFactionInfo_TG_BluFor',
-  BP_HDFactionInfo_TG_OpFor = 'BP_HDFactionInfo_TG_OpFor',
+  BP_HDFactionInfo_PMC_East = 'BP_HDFactionInfo_PMC_East',
+
+  //? Gulf War / Cold War
+
   BP_HDFactionInfo_UnitedStates = 'BP_HDFactionInfo_UnitedStates',
-  BP_OMFactionInfo_Germany = 'BP_OMFactionInfo_Germany',
-  BP_OMFactionInfo_UnitedStates = 'BP_OMFactionInfo_UnitedStates',
-  BP_OOFactionInfo_Germany = 'BP_OOFactionInfo_Germany',
+  BP_HDFactionInfo_UnitedStates_ = 'BP_HDFactionInfo_UnitedStates_',
+  BP_HDFactionInfo_Russians = 'BP_HDFactionInfo_Russians',
+  BP_HDFactionInfo_Russians_VDV = 'BP_HDFactionInfo_Russians_VDV',
+
+  //? Vietnam
+
+  BP_RSFactionInfo_USMC = 'BP_RSFactionInfo_USMC',
+  BP_RSFactionInfo_NVA = 'BP_RSFactionInfo_NVA',
+
+  //? WW2
+
   BP_OOFactionInfo_USA = 'BP_OOFactionInfo_USA',
   BP_OOFactionInfo_USRangers = 'BP_OOFactionInfo_USRangers',
+  BP_OOFactionInfo_Germany = 'BP_OOFactionInfo_Germany',
   BP_OOFactionInfo_Wehrmacht = 'BP_OOFactionInfo_Wehrmacht',
-  BP_RSFactionInfo_NVA = 'BP_RSFactionInfo_NVA',
-  BP_RSFactionInfo_USArmy = 'BP_RSFactionInfo_USArmy',
+
+  //? WW1
+
+  BP_OMFactionInfo_UnitedStates = 'BP_OMFactionInfo_UnitedStates',
+  BP_OMFactionInfo_Germany = 'BP_OMFactionInfo_Germany',
+
+  //? Training Grounds
+
+  BP_HDFactionInfo_TG_BluFor = 'BP_HDFactionInfo_TG_BluFor',
+  BP_HDFactionInfo_TG_OpFor = 'BP_HDFactionInfo_TG_OpFor',
 }
 export default Factions;
